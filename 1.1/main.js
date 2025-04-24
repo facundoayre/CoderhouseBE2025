4b046ee814741cd90dfddf3399da830a27253906
Facundo Ayre <facu.ayre@gmail.com>
2025-04-24 10:46:46 -0300

    tarea 1.1

---
+++ 1.1/main.js
@@ -0,0 +1,10 @@
+let number = 10;
+let string = "Hola";
+let bool = true;
+
+console.log(number);
+console.log(string);
+console.log(bool);
+console.log(typeof(number));
+console.log(typeof(string));
+console.log(typeof(bool));
